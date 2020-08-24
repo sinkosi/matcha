adf62009de80f42357b1bd878914e64eb5aeea95
mmamalek <mmamalek@student.wethinkcode.co.za>
2020-08-24 12:14:19 +0200

    credentials

--- api2/app/config/db.config.js
+++ api2/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	HOST: "localhost",
 	USER: "root",
-	PASSWORD: "password",
+	PASSWORD: "mosima",
 	DB: "matcha"
 };
--- api2/app/config/db.setup.js
+++ api2/app/config/db.setup.js
@@ -1,9 +1,8 @@
 var mysql = require('mysql');
-
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password"
+  password: "mosima"
 });
 
 /*
