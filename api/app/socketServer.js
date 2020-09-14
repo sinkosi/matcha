a579030fe83b1951ab5ffe537d52c4cd61e0590b
mmamalek <mmamalek@student.wethinkcode.co.za>
2020-09-14 06:43:19 +0200

    chat is cool now, but very basic

--- api/app/socketServer.js
+++ api/app/socketServer.js
@@ -40,7 +40,7 @@ module.exports = server => {
 
 		socket.on("message", (message) => {
 			console.log(message)
-			message.re = "yes"
+
 			sendMessage(message, message.to)
 		})
 		
@@ -48,7 +48,7 @@ module.exports = server => {
 
 	const sendMessage = (message, to) => {
 		if (online_users[to]){
-			console.log("testing echo")
+
 			chats.to(online_users[to]).emit("message", message)
 		}
 	}
