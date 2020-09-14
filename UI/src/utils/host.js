f9fcfe0848c077113eadb06b9aae4e139577f50c
Sibonelo <sinkosi@student.wethinkcode.co.za>
2020-09-14 15:36:26 +0200

    Central file to control api call for axios

---
+++ UI/src/utils/host.js
@@ -0,0 +1,7 @@
+//const backend_url = 'http://localhost:5000'
+const backend_url = 'http://192.168.8.102:5000'/* () => {
+    host: '192.168.8.102';
+    let port = '3000';
+}
+*/
+export { backend_url}
